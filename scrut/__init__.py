9260f927888da0562a297fec5c6bb7e1c8cfc626
Dan Trickey <dan@trickey.io>
2019-06-11 11:55:20 +0100

    Stricter models now work.

--- scrut/__init__.py
+++ scrut/__init__.py
@@ -1,7 +1,11 @@
 """Scrut: A strictly typed validator."""
 
+from .model import Config, Model
+
 __version__ = "0.1.0"
 
 __all__ = [
     "__version__",
+    "Config",
+    "Model",
 ]
--- scrut/model.py
+++ scrut/model.py
@@ -1,24 +1,31 @@
 """Validation Model Class."""
 
-from typing import Type, TypeVar
+from typing import TYPE_CHECKING, Type, TypeVar
 
-from pydantic import Extra
-from pydantic.dataclasses import dataclass
+from pydantic import BaseConfig, Extra
 
 from .source import Source
+
+if TYPE_CHECKING:
+    from dataclasses import dataclass
+else:
+    from pydantic.dataclasses import dataclass
 
 T = TypeVar("T", bound='Model')
 
 
-@dataclass
+class Config(BaseConfig):
+    """Configuration for pydantic."""
+
+    extra = Extra.forbid
+    validate_all = True
+    validate_assignment = True
+
+
+# See: https://github.com/python/mypy/issues/5406
+@dataclass(config=Config)  # type: ignore
 class Model:
     """Validation Model."""
-
-    class Config:
-        """Configuration for pydantic."""
-
-        extra = Extra.forbid
-        validate_assignment = True
 
     @classmethod
     def load(cls: Type[T], source: Source) -> T:
